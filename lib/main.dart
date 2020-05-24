682b8c67b63d185a74f388fffcbaa82631f6b3ca
Paulo <paulosoujava@gmail.com>
2020-05-24 18:46:33 -0300

    INCREMENTANDO O PROJETO

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:mobxapp/counter.dart';
+import 'package:mobxapp/screens/login_screen.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -9,55 +9,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'MobX Tutorial',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.deepPurpleAccent,
+        cursorColor: Colors.deepPurpleAccent,
+        scaffoldBackgroundColor: Colors.deepPurpleAccent,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  Counter  counter = Counter();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Observer(
-              builder: (_){
-                return Text(
-                  '${counter.count}',
-                  style: Theme.of(context).textTheme.display1,
-                );
-              },
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: counter.increment,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: LoginScreen(),
     );
   }
 }
---
+++ lib/store/login_store.dart
@@ -0,0 +1,48 @@
+
+import 'package:mobx/mobx.dart';
+
+part 'login_store.g.dart';
+
+class LoginStore = _LoginStore with _$LoginStore;
+
+abstract class _LoginStore with Store{
+  
+  _LoginStore(){
+    autorun((_){
+      print(email);
+    });
+  }
+
+  @observable
+  String email = "";
+  @action
+  void setEmail(String v ) => email = v;
+
+  @observable
+  String password = "";
+  @action
+  void setPassword(String v ) => password = v;
+
+  @observable
+  bool passwordVisible = false;
+  @action
+  void togglePasswordVisibility() => passwordVisible = !passwordVisible;
+
+  //operacoes com observable geralmente sao feitas com computed
+  /*
+  @computed
+  bool get isFormValid => email.length > 6 && password.length > 6;
+
+   OU
+   */
+  @computed
+  bool get isPasswordValid => password.length > 6;
+
+  @computed
+  bool get isEmaildValid =>
+      RegExp( r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$").hasMatch(email)
+
+  @computed
+  bool get isFormValid => isEmaildValid && isPasswordValid;
+
+}
